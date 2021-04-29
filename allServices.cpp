bbce02365ffc5e527bf1501374c336beee3abe7e
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2021-04-29 16:13:04 +0100

    Debugging compile time error

--- allServices.cpp
+++ allServices.cpp
@@ -308,63 +308,65 @@ int main(int argc, char *argv[]){
         printf("Connected. Result code %d\n", rc);
     }
 
-//    double temperature = 0;
-//    double humidity = 0;
-//    int *readings = read_dht11_dat(); // function returns an array. Index 0 and 1 are for humidity, index 2 and 3 are for temp and index 4 is the checksum
-//    int counter = 0;
-//    while(readings[0] == -1 && counter < 50){
-//        readings = read_dht11_dat(); // Errors frequently occur when reading dht sensor. Keep reading until values are returned.
-//    }
-//    if(counter == 5){
-//        std::cout << "Problem with DHT11 sensor. Check Raspberry Pi \n";
-//        return 1;
-//    }
-//    humidity = readings[0] + (readings[1]/10);
-//    temperature = readings[2] + (readings[3]/10);
-//
-//    count = 0;
-//    while(count <= 100) {
-//        if(count == 100){
-//            rapidjson::Document document_done;
-//            document_done.SetObject();
-//            rapidjson::Document::AllocatorType& allocator1 = document_done.GetAllocator();
-//            document_done.AddMember("Done", true, allocator1);
-//            std::string pub_message_done = json_to_string(document_done);
-//            rc = publish_message(pub_message_done, TOPIC_T, client_HT);
-//            rc = publish_message(pub_message_done, TOPIC_H, client_HT);
-//        }
-//        else {
-//            //Create JSON DOM document object for humidity
-//            rapidjson::Document document_humidity;
-//            document_humidity.SetObject();
-//            rapidjson::Document::AllocatorType &allocator2 = document_humidity.GetAllocator();
-//            document_humidity.AddMember("Humidity", humidity, allocator2);
-//            document_humidity.AddMember("Unit", "%", allocator2);
-//
-//            //Create JSON DOM document object for temperature
-//            rapidjson::Document document_temperature;
-//            document_temperature.SetObject();
-//            rapidjson::Document::AllocatorType &allocator3 = document_temperature.GetAllocator();
-//            document_temperature.AddMember("Temp", temperature, allocator3);
-//            document_temperature.AddMember("Unit", "C", allocator3);
-//            try {
-//                std::string pub_message_humidity = json_to_string(document_humidity);
-//                rc = publish_message(pub_message_humidity, TOPIC_H, client_HT);
-//                std::string pub_message_temperature = json_to_string(document_temperature);
-//                rc = publish_message(pub_message_temperature, TOPIC_T, client_HT);
-//            } catch (const std::exception &exc) {
-//                // catch anything thrown within try block that derives from std::exception
-//                std::cerr << exc.what();
-//            }
-//        }
-//        count = count + 1;
-//    }
-//
-//    auto end_HT = high_resolution_clock::now();
-//    std::chrono::duration<double> timer_HT = end_HT-start_HT;
-//    outfile.open("piResultsCppMono.txt", std::ios_base::app); // append to the results text file
-//    outfile << "Humidity and temperature publisher runtime = " << timer_HT.count() << "\n";
-//    std::cout << "Humidity and temperature runtime = " << timer_HT.count() << "\n";
+    double temperature = 0;
+    double humidity = 0;
+    int *readings = read_dht11_dat(); // function returns an array. Index 0 and 1 are for humidity, index 2 and 3 are for temp and index 4 is the checksum
+    int counter = 0;
+    while(readings[0] == -1 && counter < 8){
+        readings = read_dht11_dat(); // Errors frequently occur when reading dht sensor. Keep reading until values are returned.
+        counter = counter + 1;
+    }
+    if(counter == 5
+    8){
+        std::cout << "Problem with DHT11 sensor. Check Raspberry Pi \n";
+        return 1;
+    }
+    humidity = readings[0] + (readings[1]/10);
+    temperature = readings[2] + (readings[3]/10);
+
+    count = 0;
+    while(count <= 100) {
+        if(count == 100){
+            rapidjson::Document document_done;
+            document_done.SetObject();
+            rapidjson::Document::AllocatorType& allocator1 = document_done.GetAllocator();
+            document_done.AddMember("Done", true, allocator1);
+            std::string pub_message_done = json_to_string(document_done);
+            rc = publish_message(pub_message_done, TOPIC_T, client_HT);
+            rc = publish_message(pub_message_done, TOPIC_H, client_HT);
+        }
+        else {
+            //Create JSON DOM document object for humidity
+            rapidjson::Document document_humidity;
+            document_humidity.SetObject();
+            rapidjson::Document::AllocatorType &allocator2 = document_humidity.GetAllocator();
+            document_humidity.AddMember("Humidity", humidity, allocator2);
+            document_humidity.AddMember("Unit", "%", allocator2);
+
+            //Create JSON DOM document object for temperature
+            rapidjson::Document document_temperature;
+            document_temperature.SetObject();
+            rapidjson::Document::AllocatorType &allocator3 = document_temperature.GetAllocator();
+            document_temperature.AddMember("Temp", temperature, allocator3);
+            document_temperature.AddMember("Unit", "C", allocator3);
+            try {
+                std::string pub_message_humidity = json_to_string(document_humidity);
+                rc = publish_message(pub_message_humidity, TOPIC_H, client_HT);
+                std::string pub_message_temperature = json_to_string(document_temperature);
+                rc = publish_message(pub_message_temperature, TOPIC_T, client_HT);
+            } catch (const std::exception &exc) {
+                // catch anything thrown within try block that derives from std::exception
+                std::cerr << exc.what();
+            }
+        }
+        count = count + 1;
+    }
+
+    auto end_HT = high_resolution_clock::now();
+    std::chrono::duration<double> timer_HT = end_HT-start_HT;
+    outfile.open("piResultsCppMono.txt", std::ios_base::app); // append to the results text file
+    outfile << "Humidity and temperature publisher runtime = " << timer_HT.count() << "\n";
+    std::cout << "Humidity and temperature runtime = " << timer_HT.count() << "\n";
 
     while(session_status != "Done"){ // Continue listening for messages until end of session
         //Do nothing
