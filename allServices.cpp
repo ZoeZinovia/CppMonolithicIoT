1f166e6aa82a5fb864f0d0a5a58d7f1a1ccb1ca5
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2021-04-29 16:00:59 +0100

    Debugging compile time error

--- allServices.cpp
+++ allServices.cpp
@@ -239,12 +239,12 @@ int main(int argc, char *argv[]){
     auto start_pir = high_resolution_clock::now(); // Starting timer
 
     MQTTClient client_pir;
-    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+    MQTTClient_connectOptions conn_opts_pir = MQTTClient_connectOptions_initializer;
     MQTTClient_create(&client_pir, ADDRESS, CLIENTID_PIR, MQTTCLIENT_PERSISTENCE_NONE, NULL);
-    conn_opts.keepAliveInterval = 20;
-    conn_opts.cleansession = 1;
+    conn_opts_pir.keepAliveInterval = 20;
+    conn_opts_pir.cleansession = 1;
 
-    if ((rc = MQTTClient_connect(client_pir, &conn_opts)) != MQTTCLIENT_SUCCESS){
+    if ((rc = MQTTClient_connect(client_pir, &conn_opts_pir)) != MQTTCLIENT_SUCCESS){
         printf("Failed to connect, return code %d\n", rc);
         exit(EXIT_FAILURE);
     } else{
@@ -294,13 +294,13 @@ int main(int argc, char *argv[]){
     auto start_HT = high_resolution_clock::now(); // Starting timer
 
     MQTTClient client_HT;
-    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+    MQTTClient_connectOptions conn_opts_HT = MQTTClient_connectOptions_initializer;
 
     MQTTClient_create(&client_HT, ADDRESS, CLIENTID_H_T, MQTTCLIENT_PERSISTENCE_NONE, NULL);
-    conn_opts.keepAliveInterval = 20;
-    conn_opts.cleansession = 1;
+    conn_opts_HT.keepAliveInterval = 20;
+    conn_opts_HT.cleansession = 1;
 
-    if ((rc = MQTTClient_connect(client_HT, &conn_opts)) != MQTTCLIENT_SUCCESS)
+    if ((rc = MQTTClient_connect(client_HT, &conn_opts_HT)) != MQTTCLIENT_SUCCESS)
     {
         printf("Failed to connect, return code %d\n", rc);
         exit(EXIT_FAILURE);
