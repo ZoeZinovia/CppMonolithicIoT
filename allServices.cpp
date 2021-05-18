8c70b0068b842a29c820ade63d86c3d285a2f57f
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2021-05-18 11:10:17 +0100

    checking allservices one more time

--- allServices.cpp
+++ allServices.cpp
@@ -278,7 +278,7 @@ int main(int argc, char* argv[])
     auto dhtEnd = high_resolution_clock::now();
     std::chrono::duration<double> dhtTimer;
     while(count <= num_iterations) {
-        std::cout << count;
+        std::cout << count << " ";
         if(count == num_iterations){
             rapidjson::Document document_done;
             document_done.SetObject();
